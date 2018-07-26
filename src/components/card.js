c4547f94db7ec978726ff346b4beda35e7baf4cc
Jared Grant <jared.grant@canyonsdistrict.org>
2018-07-26 11:07:42 -0600

    needs work

--- src/components/card.js
+++ src/components/card.js
@@ -4,31 +4,35 @@ import Input from './input';
 
 class Card extends Component {
 
-    constructor() {
-        super()
+   constructor() {
+       super()
 
-        this.state = {
-            color: '',
-            pluralNoun: ''
-        }
+       this.state = {
+           color: '',
+           pluralNoun: '',
+           adjectiveOne: '',
+           celebOne: ''
+       }
 
-        this.handleInputChange = this.handleInputChange.bind(this);
-    } 
+       this.handleInputChange = this.handleInputChange.bind(this);
+   }
 
-    handleInputChange(event) {
-        this.setState({ [event.target.name]: event.target.value })
-        console.log(this.state);
-    }
+   handleInputChange(event) {
+       this.setState({ [event.target.name]: event.target.value })
+       console.log(this.state);
+   }
 
-    render() {
-        return (
-            <div className="card">
-                <h1>{this.state.color}</h1>
-                { Input('Color', this.state.color, this.handleInputChange, 'color')}
-                { Input('Plural Noun', this.state.pluralNoun, this.handleInputChange, 'pluralNoun') }
-            </div>
-        )
-    }
+   render() {
+       return (
+           <div className="card">
+               <h1>{this.state.color}</h1>
+               { Input('Color', this.state.color, this.handleInputChange, 'color')}
+               { Input('Plural Noun', this.state.pluralNoun, this.handleInputChange, 'pluralNoun') }
+               { Input('Adjective', this.state.adjective, this.handleInputChange, 'adjectiveOne')}
+               { Input('Celebrity', this.state.celebOne, this.handleInputChange, 'celebOne') }
+           </div>
+       )
+   }
 }
 
 export default Card;
