c6592ac0d159dc0c93a77592b58422c63b01ff4c
hawaiiangal <40636450+hawaiiangal@users.noreply.github.com>
2018-07-31 08:45:46 -0600

    added header_border

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Header = () => {
     return (
         <div className="header">
-            <div> className="header_skew"></div>
+            <div className="header_skew"></div>
+            <div classname="header_border"></div>
             <h1>Bottega Madlibs</h1>
             <p>Fill out the fields below and click the generate button to see the Mad Lib story.</p>
         </div>
